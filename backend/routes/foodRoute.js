7417436a27f9532e8f744ca4eb38d37de1feed91
Vipul1312 <vipulgupta1312002@gmail.com>
2026-05-08 16:06:14 +0530

    changes updated

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,21 +1,29 @@
 import express from 'express'
-import { addFood , listFood,removeFood } from '../controllers/foodController.js'
+import { addFood, listFood, removeFood } from '../controllers/foodController.js'
 import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
 
 const foodRouter = express.Router();
 
-// Image storage engine
-const storage = multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'food-delivery',
+        allowed_formats: ['jpg', 'jpeg', 'png'],
     }
-})
+});
 
-const upload = multer({storage:storage})
+const upload = multer({ storage: storage });
 
-foodRouter.post("/add",upload.single("image"),addFood)
-foodRouter.get("/list",listFood)
-foodRouter.post("/remove",removeFood)
+foodRouter.post("/add", upload.single("image"), addFood)
+foodRouter.get("/list", listFood)
+foodRouter.post("/remove", removeFood)
 
 export default foodRouter;
